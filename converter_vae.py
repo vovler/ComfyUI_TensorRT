456af7a80a91cd1621d8c0c8eb013b6b7dd31cf4
vovler <vovler.lupu1@gmail.com>
2025-06-24 19:51:43 +0000

    Remove PREFER_PRECISION_CONSTRAINTS flag from build_engine function to enhance flexibility in TensorRT engine configuration.

--- converter_vae.py
+++ converter_vae.py
@@ -69,7 +69,6 @@ def build_engine(onnx_file_path, engine_file_path, min_shape, opt_shape, max_sha
     print("Configuring TensorRT builder for maximum performance on specific target...")
 
     config.set_flag(trt.BuilderFlag.FP16)
-    config.set_flag(trt.BuilderFlag.PREFER_PRECISION_CONSTRAINTS)
     config.set_flag(trt.BuilderFlag.DIRECT_IO)
     config.set_flag(trt.BuilderFlag.WEIGHT_STREAMING)
     config.hardware_compatibility_level = trt.HardwareCompatibilityLevel.NONE
